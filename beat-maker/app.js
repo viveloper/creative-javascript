510b9a525b44ef7fba67435bb24c79fdd151a603
viveloper <ucsik1234@gmail.com>
2020-06-09 10:23:03 +0900

    updated beat-maker

--- beat-maker/app.js
+++ beat-maker/app.js
@@ -1,1 +1,35 @@
-document.addEventListener('DOMContentLoaded', () => {});
+class DrumKit {
+  constructor() {
+    this.pads = document.querySelectorAll('.pad');
+    this.playBtn = document.querySelector('.play');
+    this.kickAudio = document.querySelector('.kick-sound');
+    this.snareAudio = document.querySelector('.snare-sound');
+    this.hihatAudio = document.querySelector('.hihat-sound');
+    this.index = 0;
+    this.bpm = 150;
+
+    this.playBtn.addEventListener('click', this.start.bind(this));
+    this.pads.forEach((pad) => {
+      pad.addEventListener('click', this.activePad);
+    });
+  }
+  activePad() {
+    console.log(this.classList.toggle('active'));
+  }
+  repeat() {
+    const step = this.index % 8;
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    console.log(activeBars);
+    this.index++;
+  }
+  start() {
+    const interval = (60 / this.bpm) * 1000;
+    setInterval(() => {
+      this.repeat();
+    }, interval);
+  }
+}
+
+const drumKit = new DrumKit();
+
+// drumKit.playBtn.addEventListener('click', () => drumKit.start());
